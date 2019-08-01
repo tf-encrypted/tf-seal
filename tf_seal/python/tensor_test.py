a24e3908d8a88ed2a1a5ab02c75181cb27bd4685
Justin Patriquin <justinpatriquin@gmail.com>
2019-08-01 10:53:30 -0300

    log reg example

--- tf_seal/python/tensor_test.py
+++ tf_seal/python/tensor_test.py
@@ -5,6 +5,7 @@ from tensorflow.python.platform import test
 
 from tf_seal.python.ops.seal_ops import seal_encrypt, seal_key_gen
 from tf_seal.python.tensor import convert_to_tensor
+from tf_seal.python.tensor import constant
 
 
 class EvaluationTest(test.TestCase):
@@ -86,6 +87,24 @@ class ArithmeticTest(test.TestCase):
 
   def test_matmul_plain(self):
     self._core_matmul_test(plain=True)
+
+
+  def test_log_reg(self):
+    public_keys, secret_key = seal_key_gen(gen_relin=True, gen_galois=True)
+
+    # encrypted input -> tf_seal.Tensor
+    a = constant(np.random.normal(size=(10, 10)).astype(np.float32), secret_key, public_keys)
+    print(a)
+
+    # public weights
+    b = np.random.normal(size=(10, 10)).astype(np.float32)
+
+    # TODO tfs.matmul(a, b)????
+    c = a.matmul(b)
+    print(c)
+
+    with tf.Session() as sess:
+      sess.run(c)
 
 
 class ConvertTest(test.TestCase):
