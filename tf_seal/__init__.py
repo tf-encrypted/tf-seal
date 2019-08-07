d5c1611fc2a15e7671d72f8629a26bd8d732c41a
Morten Dahl <mortendahlcs@gmail.com>
2019-08-07 15:01:52 +0200

    matmul example

--- tf_seal/__init__.py
+++ tf_seal/__init__.py
@@ -1,4 +1,5 @@
 from tf_seal.python.tensor import constant
+from tf_seal.python.tensor import convert_to_tensor
 from tf_seal.python.tensor import matmul
 from tf_seal.python.tensor import poly_eval
 from tf_seal.python.ops.seal_ops import seal_key_gen
